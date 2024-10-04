5eb64a66dc655388f54f966da4964475add142b6
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-10-04 18:51:21 +0530

    04-10-2024 all data uploaded

---
+++ module2-essentials-javascript/what is function in javascript/normal-function/js/address.js
@@ -0,0 +1,20 @@
+function add1()
+{
+    var ad1="150 feet ring road near jalaram petrol pump behinf naptune tower 4th floor Rajkot-360001";
+    document.write(ad1);
+}
+function add2()
+{
+    var ad2="near gondal chokadi petrol pump behinf naptune tower 4th floor Rajkot-360001";
+    document.write(ad2);
+}
+function add3()
+{
+    var ad3="near atal sarovar pump behinf naptune tower 4th floor Rajkot-360001";
+    document.write(ad3);
+}
+function add4()
+{
+    var ad4="indira circle petrol pump behinf naptune tower 4th floor Rajkot-360001";
+    document.write(ad4);
+}
