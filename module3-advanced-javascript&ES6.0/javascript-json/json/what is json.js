7cbb8a377371087dd2bd59c0507e810c8c0ec701
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-11-22 19:05:25 +0530

    22-12-2024 all data uploaded

---
+++ module3-advanced-javascript&ES6.0/javascript-json/json/what is json.js
@@ -0,0 +1,36 @@
+/* 
+json stands for javascript object notations 
+json is light weight
+json is used to integrate api data 
+json always load on broswers using .json extension 
+
+examples :  {
+    
+       "users":[
+         
+            {
+              "id":1001,
+              "name":"raj singh",
+              "age":34,
+              "salary":89500,
+              "address":"150 feet ing road rajkot"
+            },
+
+             {
+              "id":1002,
+              "name":"kriti singh",
+              "age":30,
+              "salary":129500,
+              "address":"150 feet ing road rajkot"
+            }
+       
+       
+       ]
+
+
+}
+
+
+
+
+*/
